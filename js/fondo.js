590a13867955ee692d9825fbdf783c4cefa25bf4
miguelgg295650 <158503448+miguelgg295650@users.noreply.github.com>
2024-11-14 13:01:22 +0100

    Add files via upload

--- js/fondo.js
+++ js/fondo.js
@@ -1,25 +1,40 @@
-class Fondo{
-    getImages(){
-        var flickrAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
-        $.getJSON(flickrAPI, 
-            {
-                tags: "F1",
-                tagmode: "any",
-                format: "json"
-            })
-        .done(function(data) {
-            $.each(data.items, function() {
-                if (data.items.length > 0) { 
-                    document.getElementsByClassName("FotoInicio")[0].src = data.items[0].media.m; 
-                }else{
-                    document.write("<h1>No fotos</h1>");
+class Fondo {
+    getImages() {
+        console.log("Image URL:"); 
+        const flickrAPI = "https://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
+        $.getJSON(flickrAPI, {
+            tags: "landscape",
+            tagmode: "any",
+            format: "json",
+        }).done(function(data) {
+            if (data.items.length > 0) {
+                const r = Math.floor(Math.random() * data.items.length);
+
+                const imageUrl = data.items[r].media.m;
+
+                if (imageUrl) {
+                    
+                    $('body').css('background-image', 'url(' + imageUrl + ')');
+                    $('body').css('background-size', 'cover');
+                    $('body').css('background-position', 'center');
+                    $('body').css('background-attachment', 'fixed');
+                    $('body').css('background-repeat', 'no-repeat');
+                    $('body').css('height', '100vh');
+                } else {
+                    console.log("No valid image URL found.");
                 }
-                
-            })})
+            
+            } else {
+                document.write("<h1>No fotos</h1>");
+            }
+        });
     }
-   
- 
 }
-var f = new Fondo();
+
+
+const f = new Fondo();
 f.getImages();
-   
+
+
+
+    
